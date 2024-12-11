c9bab0299dd429c5a35a8eeb601b8b524db0f2ea
BRUNELLIERE Erwan <ebrunelliere@softwaymedical.fr>
2024-12-11 16:33:24 +0000

    ......

--- d7/src/main.rs
+++ d7/src/main.rs
@@ -1,31 +1,34 @@
 use regex::Regex;
 use std::fs;
 
-fn get_results(&mut numbers: &mut Vec<u128>) -> Vec<u128> {
-    if numbers.len() == 1 {
-        return numbers;
+fn concat(a: u128, b: u128) -> u128 {
+    (a.to_string() + &b.to_string()).parse::<u128>().unwrap()
+}
+
+fn get_results(first_value: u128, from: usize, numbers: &Vec<u128>) -> Vec<u128> {
+    if numbers.len() == from {
+        return vec![first_value];
     }
 
-    let mut add: Vec<u128> = vec![numbers[0] + numbers[1]];
-    let mut mul: Vec<u128> = vec![numbers[0] * numbers[1]];
-    add.append(&mut numbers[1..].to_vec());
-    mul.append(&mut numbers[1..].to_vec());
-
-    let mut r1 = get_results(&mut add);
-    let mut r2 = get_results(&mut mul);
-
+    let mut r1 = get_results(first_value + numbers[from], from+1,&numbers);
+    let mut r2 = get_results(first_value * numbers[from], from+1,&numbers);
+    let mut r3 = get_results(concat(first_value, numbers[from]), from+1,&numbers);
+    
     r1.append(&mut r2);
+    r1.append(&mut r3);
 
     return r1;
 }
 
 fn main() {
-    println!("Day 1");
-    let file_path = "./example.txt";
+    println!("Day 7");
+    let file_path = "./input.txt";
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     let re = Regex::new(r"(?m)^(\d+):\s+(.+)").unwrap();
+
+    let mut total: u128 = 0;
 
     for (_, [m, l]) in re.captures_iter(&contents).map(|c| c.extract()) {
         let wanted = m.parse::<u128>().unwrap();
@@ -34,11 +37,12 @@ fn main() {
             .map(|x| x.parse::<u128>().unwrap())
             .collect();
 
-        println!("{}", wanted);
-        println!("{:?}", numbers);
 
-        let result = numbers[0];
+        let results = get_results(numbers[0], 1, &numbers);
 
-        for n in 1..numbers.len() {}
+        if results.contains(&wanted) {
+            total += wanted;
+        }
     }
+    println!("Part 1-2 : {}", total);
 }
